ad583d0635612ade0d5cd74a1c72a3b49109f6a0
Pau Cabaneros <pau.cabaneros@gmail.com>
2022-07-01 09:14:56 +0200

    add functions to load datafiles

---
+++ dtuprosys/datasets/_base.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads the train data.
+    """
+    train_spectra = pd.read_csv('./dtuprosys/datasets/data/train_spectra.csv')
+    train_hplc = pd.read_csv('./dtuprosys/datasets/data/train_hplc.csv')
+
+    return train_spectra, train_hplc
+
+
+def load_fermentation_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads the fermentation data.
+    """
+    fermentation_spectra = pd.read_csv('./dtuprosys/datasets/data/fermentation_spectra.csv')
+    fermentation_hplc = pd.read_csv('./dtuprosys/datasets/data/fermentation_hplc.csv')
+
+    return fermentation_spectra, fermentation_hplc
