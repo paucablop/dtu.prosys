e37549987d8e66c5692ea8482be041c7d3e3780d
Pau Cabaneros <pau.cabaneros@gmail.com>
2022-07-02 07:22:06 +0200

    update version of jinja2 in docstring2md

--- dtuprosys/datasets/_base.py
+++ dtuprosys/datasets/_base.py
@@ -3,6 +3,7 @@ import pandas as pd
 def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Loads the train data.
+    @return: tuple with spectra and hplc data.
     """
     train_spectra = pd.read_csv('./dtuprosys/datasets/data/train_spectra.csv')
     train_hplc = pd.read_csv('./dtuprosys/datasets/data/train_hplc.csv')
@@ -13,6 +14,7 @@ def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
 def load_fermentation_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Loads the fermentation data.
+    @return: tuple with spectra and hplc data.
     """
     fermentation_spectra = pd.read_csv('./dtuprosys/datasets/data/fermentation_spectra.csv')
     fermentation_hplc = pd.read_csv('./dtuprosys/datasets/data/fermentation_hplc.csv')
