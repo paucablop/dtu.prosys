79772eb9833c244c3b3950bc3318052397c0516a
Pau Cabaneros <pau.cabaneros@gmail.com>
2022-07-02 07:30:50 +0200

    update docstrings

--- dtuprosys/datasets/_base.py
+++ dtuprosys/datasets/_base.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+
 def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Loads the train data.
-    @return: tuple with spectra and hplc data.
+    @return train_spectra: spectra in the training data.
+    @return train_hplc: hplc in the training data.
     """
-    train_spectra = pd.read_csv('./dtuprosys/datasets/data/train_spectra.csv')
-    train_hplc = pd.read_csv('./dtuprosys/datasets/data/train_hplc.csv')
+    train_spectra = pd.read_csv("./dtuprosys/datasets/data/train_spectra.csv")
+    train_hplc = pd.read_csv("./dtuprosys/datasets/data/train_hplc.csv")
 
     return train_spectra, train_hplc
 
@@ -14,9 +16,12 @@ def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
 def load_fermentation_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Loads the fermentation data.
-    @return: tuple with spectra and hplc data.
+    @return fermentation_spectra: spectra measured during the fermentation.
+    @return fermentation_hplc: hplc measured during the fermentation.
     """
-    fermentation_spectra = pd.read_csv('./dtuprosys/datasets/data/fermentation_spectra.csv')
-    fermentation_hplc = pd.read_csv('./dtuprosys/datasets/data/fermentation_hplc.csv')
+    fermentation_spectra = pd.read_csv(
+        "./dtuprosys/datasets/data/fermentation_spectra.csv"
+    )
+    fermentation_hplc = pd.read_csv("./dtuprosys/datasets/data/fermentation_hplc.csv")
 
     return fermentation_spectra, fermentation_hplc
--- dtuprosys/preprocessing/preprocessing.py
+++ dtuprosys/preprocessing/preprocessing.py
@@ -4,23 +4,50 @@ import pandas as pd
 
 
 class RangeCut:
+    """
+    Cuts a range from a dataframe.
+    """
     def __init__(self, start: int, end: int):
+        """
+        Constructor.
+        @param start: start index
+        @param end: end index
+        """
         self.start = start
         self.end = end
 
     def apply_to(self, x: pd.DataFrame) -> pd.DataFrame:
+        """
+        Applies the cut to the dataframe.
+        @param x: dataframe
+        @return: dataframe with cut
+        """
         return x.iloc[:, self.start : self.end]
 
 
 class Derivative:
+    """
+    Calculates the derivative of a each row in a dataframe.
+    """
     def __init__(
         self, derivative_order: int, window_length: int = 15, polynomial_order: int = 1
     ):
+        """
+        Constructor.
+        @param derivative_order: derivative order
+        @param window_length: window length
+        @param polynomial_order: polynomial order
+        """
         self.derivative_order = derivative_order
         self.window_length = window_length
         self.polynomial_order = polynomial_order
 
     def apply_to(self, x: pd.DataFrame) -> pd.DataFrame:
+        """
+        Applies the derivative to the dataframe.
+        @param x: dataframe
+        @return: dataframe with derivative
+        """
         return savgol_filter(
             x, self.window_length, self.polynomial_order, deriv=self.derivative_order
         )
