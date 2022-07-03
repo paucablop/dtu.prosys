9e9c76c1bda82786d95e0a055b065e121672787c
Pau Cabaneros <pau.cabaneros@gmail.com>
2022-07-03 22:52:04 +0200

    update directories

--- dtuprosys/datasets/_base.py
+++ dtuprosys/datasets/_base.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 
+PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """
@@ -7,9 +9,9 @@ def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     @return train_spectra: spectra in the training data.
     @return train_hplc: hplc in the training data.
     """
-    train_spectra = pd.read_csv(r"./dtuprosys/datasets/data/train_spectra.csv")
+    train_spectra = pd.read_csv(PACKAGE_DIRECTORY + "/data/train_spectra.csv")
     train_spectra.columns = train_spectra.columns.astype(float)
-    train_hplc = pd.read_csv(r"./dtuprosys/datasets/data/train_hplc.csv")
+    train_hplc = pd.read_csv(PACKAGE_DIRECTORY + "/data/train_hplc.csv")
 
     return train_spectra, train_hplc
 
@@ -21,9 +23,9 @@ def load_fermentation_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     @return fermentation_hplc: hplc measured during the fermentation.
     """
     fermentation_spectra = pd.read_csv(
-        r"./dtuprosys/datasets/data/fermentation_spectra.csv"
+            PACKAGE_DIRECTORY + "/data/fermentation_spectra.csv"
     )
     fermentation_spectra.columns = fermentation_spectra.columns.astype(float)
-    fermentation_hplc = pd.read_csv(r"./dtuprosys/datasets/data/fermentation_hplc.csv")
+    fermentation_hplc = pd.read_csv(PACKAGE_DIRECTORY + "/data/fermentation_hplc.csv")
 
     return fermentation_spectra, fermentation_hplc
